b8a3c84150cfcfba6548360001a4682e0e0413ca
Gustavo Ribeiro Garcia <102680004+Garciaza1@users.noreply.github.com>
2024-04-23 11:24:51 -0300

    commit-1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h1>VAI DURMI GATINHAAAAAAAAAAA</h1>";
+
+// <?php
+
+// require_once("vendor/autoload.php");
+
+// use GymSolution\System\Router;
+
+// session_start();
+
+// Router::dispatch();
---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//APP
+define('APP_NAME',  '##');
+
+define('IMAGE_PATH', 'assets/imgs/');
+define('CSS_PATH', 'assets/css/style.css');
+define('JS_PATH', 'assets/scripts/app.js');
+
+
+//bd
+define('MYSQL_HOST',        'localhost');
+define('MYSQL_DATABASE',    'gymsolution');
+define('MYSQL_USERNAME',    'root');
+define('MYSQL_PASSWORD',    '');
+define('aes_cript',    'rH$v3KRPiWw(41j)LpaDuYG(%zUcvs+#');
